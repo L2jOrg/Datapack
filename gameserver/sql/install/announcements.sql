3d563501a3eba30c7cf48721c95ddfbd38646fc4
Alisson Oliveira <joe.alisson@gmail.com>
2019-03-26 13:03:35 -0300

    Reorganize structures + clean up
    Signed-off-by: Alisson Oliveira <joe.alisson@gmail.com>

---
+++ gameserver/sql/install/announcements.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS `announcements`;
+CREATE TABLE IF NOT EXISTS `announcements` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL,
+  `initial` bigint(20) NOT NULL DEFAULT 0,
+  `delay` bigint(20) NOT NULL DEFAULT 0,
+  `repeat` int(11) NOT NULL DEFAULT 0,
+  `author` text NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO announcements (`type`, `author`, `content`) VALUES 
+(0, 'L2j', 'Thanks for using L2j Org!');
