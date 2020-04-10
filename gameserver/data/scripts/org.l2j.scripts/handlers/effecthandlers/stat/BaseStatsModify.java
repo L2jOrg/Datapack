acfc5bdcd1693dfb097384397ce7e8648abd410e
Alisson Oliveira <joe.alisson@gmail.com>
2020-04-10 20:06:05 -0300

    Initial Transformation System
    Signed-off-by: Alisson Oliveira <joe.alisson@gmail.com>
    
    #113

--- gameserver/data/scripts/org.l2j.scripts/handlers/effecthandlers/stat/BaseStatsModify.java
+++ gameserver/data/scripts/org.l2j.scripts/handlers/effecthandlers/stat/BaseStatsModify.java
@@ -1,40 +1,42 @@
-package handlers.effecthandlers;
+package handlers.effecthandlers.stat;
 
+import org.l2j.gameserver.engine.skill.api.Skill;
 import org.l2j.gameserver.engine.skill.api.SkillEffectFactory;
 import org.l2j.gameserver.model.StatsSet;
 import org.l2j.gameserver.model.actor.Creature;
 import org.l2j.gameserver.model.effects.AbstractEffect;
-import org.l2j.gameserver.engine.skill.api.Skill;
 import org.l2j.gameserver.model.stats.BaseStats;
 
-/**
- * @author Sdw
- * @author JoeAlisson
- * */
-public class StatUp extends AbstractEffect {
-    private final BaseStats stat;
-    private final double power;
+import java.util.EnumSet;
 
-    private StatUp(StatsSet params) {
-        power = params.getDouble("power", 0);
-        stat = params.getEnum("type", BaseStats.class, BaseStats.STR);
+/**
+ * @author JoeAlisson
+ */
+public class BaseStatsModify extends AbstractEffect {
+
+    private final double power;
+    private final EnumSet<BaseStats> stats;
+
+    private BaseStatsModify(StatsSet data) {
+        power = data.getDouble("power", 0);
+        stats = data.getStringAsEnumSet("types", BaseStats.class);
     }
 
     @Override
     public void pump(Creature effected, Skill skill) {
-        effected.getStats().mergeAdd(stat.getStat(), power);
+        stats.forEach(stat -> effected.getStats().mergeAdd(stat.getStat(), power));
     }
 
     public static class Factory implements SkillEffectFactory {
 
         @Override
         public AbstractEffect create(StatsSet data) {
-            return new StatUp(data);
+            return new BaseStatsModify(data);
         }
 
         @Override
         public String effectName() {
-            return "base-stat";
+            return "base-stats";
         }
     }
 }
